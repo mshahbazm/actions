cc2c7c059935a94890f9004791bfc6af6be2bb07
Nomi <naumanahmed19@gmail.com>
2022-06-25 19:18:43 +0200

    Update bash-script.sh

--- bash-script.sh
+++ bash-script.sh
@@ -2,11 +2,3 @@
 
 echo "Hello $1"
 echo "Current time is $2"
-
-if [[ $3 == 1 ]]; then
-  echo "Execute Function-1"
-elif [[ $3 == 2 ]]; then
-  echo "Execute Function-2"
-else
-  echo "Execute Function-3"
-fi
